6af810f11c94f3752b4d11a6925f3f60c279473f
Nikolay <nick.zhigalin@gmail.com>
2019-05-29 00:57:46 +0300

    Правки settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'API.apps.ApiConfig',
+    'API',
     'rest_framework',
     'corsheaders',
 ]
