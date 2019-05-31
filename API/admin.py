0f928e9a5c57382a2fb3c94e3940a4192da61abf
Nikolay <nick.zhigalin@gmail.com>
2019-06-01 00:11:09 +0300

    Фикс регистрации в админке

--- API/admin.py
+++ API/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from api.models import Employee, Organization, Attachment
 
 # Register your models here.
-admin.site.register(Employee, Organization, Attachment)
+admin.site.register(Employee)
+admin.site.register(Organization)
+admin.site.register(Attachment)
