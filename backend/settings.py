f55a2c3a5c1b5c7e27506e1d25094d8d9c26b727
Nikolay <nick.zhigalin@gmail.com>
2019-06-09 12:21:23 +0300

    Добавлен rest-auth

--- backend/settings.py
+++ backend/settings.py
@@ -36,9 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
     'corsheaders',
+    'api',
 ]
 
 MIDDLEWARE = [
