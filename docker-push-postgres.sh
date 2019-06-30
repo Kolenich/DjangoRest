f6e227c9c6599eda853a382e09b025110f2a2ac0
Nikolay <nick.zhigalin@gmail.com>
2019-06-30 14:26:15 +0300

    Dockerfile'ы перенесены в другую папку

--- docker-push-postgres.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-docker build -t postgres -f Dockerfile.postgres .
-docker tag postgres kolenich/postgres
-docker push kolenich/postgres
--- docker-push-server.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-docker build -t server -f Dockerfile.server .
-docker tag server kolenich/server
-docker push kolenich/server
