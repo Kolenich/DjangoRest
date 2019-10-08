2a7e62ec8e5dc29858acb660ebde3b4eed33a871
Nikolay <nick.zhigalin@gmail.com>
2019-10-08 10:02:46 +0300

    Переход на postgresql

--- backend/settings_dev.py
+++ backend/settings_dev.py
@@ -13,6 +13,16 @@ DATABASES['default'] = {
     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 }
 
+if os.environ.get('USER') in ('nikolay',):
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'nikolay',
+        'PASSWORD': 'Crowel_16',
+        'HOST': 'localhost',
+        'PORT': '5435',
+    }
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 CELERY_BROKER_URL = 'sqla+sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
