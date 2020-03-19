06a24ea993a3f03666887121da5ac9d0a47e9675
Nikolay <nick.zhigalin@gmail.com>
2020-03-19 11:13:22 +0300

    Добавлен сигнал на удаление аватара при удалении профиля пользователя

--- common_models_app/signals/attachment.py
+++ common_models_app/signals/attachment.py
@@ -3,8 +3,8 @@ import os
 
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
-from django_filters.conf import settings
 
+from backend.settings import BASE_DIR
 from common_models_app.models import Attachment
 from lib.functions import delete_empty_dirs
 
@@ -13,4 +13,4 @@ from lib.functions import delete_empty_dirs
 def delete_file_from_host(sender, instance: Attachment, **kwargs):
     """Удаляет файл с хоста после удаления связанной модели."""
     instance.file.delete(False)
-    delete_empty_dirs(os.path.join(settings.BASE_DIR, 'media'))
+    delete_empty_dirs(os.path.join(BASE_DIR, 'media'))
